eec4f9915b2d330241b5fb2ace92a54052562494
Nicholas Lambert <59819334+nnlambert@users.noreply.github.com>
2022-12-29 23:08:59 -0800

    Update ContentView.swift
    Completed code for first question screen

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -28,7 +28,42 @@ struct ContentView: View {
                     Button(action: {
                         print("Tapped on Choice 1")
                         }, label: {
-                            Text("Change")
+                            Text("Ant")
+                                .font(.body)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .border(accentColor, width: 4)
+                                })
+                    Button(action: {
+                            print("Tapped on Choice 2")
+                        }, label: {
+                            Text("Beetle")
+                                .font(.body)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .border(accentColor, width: 4)
+                        })
+                        Button(action: {
+                            print("Tapped on Choice 3")
+                        }, label: {
+                            Text("Moth")
+                                .font(.body)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .border(accentColor, width: 4)
+                        })
+                        Button(action: {
+                            print("Tapped on Choice 4")
+                        }, label: {
+                            Text("Fly")
+                                .font(.body)
+                                .bold()
+                                .multilineTextAlignment(.center)
+                                .padding()
+                                .border(accentColor, width: 4)
                         })
                 }
             }
