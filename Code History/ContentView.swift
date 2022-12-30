d2d4e26ef48328320c305ad10aea57f5b071bd0d
Nicholas Lambert <59819334+nnlambert@users.noreply.github.com>
2022-12-29 22:36:50 -0800

    Basis for app including first view

--- Code History/ContentView.swift
+++ Code History/ContentView.swift
@@ -5,17 +5,35 @@
 //  Created by Nicholas Lambert on 12/16/22.
 //
 
+
 import SwiftUI
 
 struct ContentView: View {
+    let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            mainColor.ignoresSafeArea()
+            VStack {
+                Text("1 / 10")
+                .font(.callout)
+                .multilineTextAlignment(.leading)
+                .padding()
+                Text("What was the first computer bug?")
+                .font(.largeTitle)
+                .bold()
+                .multilineTextAlignment(.leading)
+                Spacer()
+                HStack {
+                    Button(action: {
+                        print("Tapped on Choice 1")
+                        }, label: {
+                            Text("Button")
+                        })
+                }
+            }
         }
-        .padding()
+        .foregroundColor(.white)
     }
 }
 
